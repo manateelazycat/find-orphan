cc896fe01fa348702c553ce73e894f4b18b46889
Andy Stewart <lazycat.manatee@gmail.com>
2021-11-27 11:31:30 +0800

    Init.

---
+++ README.md
@@ -0,0 +1,24 @@
+# What is find-orphan.el ?
+When we complete a large programming project, we often need to refactor the code to remove the unused code:
+1. Find function definition
+2. Pass function name to ripgrep then search in current project
+3. Remove function code if not reference in project
+
+Repeat the above work in a large project will spend a lot of time.
+
+Now, you can use command ```find-orphan-function-in-directory```, it will print all orphan functions in current project.
+
+## Installation
+1. Install tree-sitter first: https://emacs-tree-sitter.github.io/installation/
+2. Install [ripgrep](https://github.com/BurntSushi/ripgrep)
+3. Clone or download this repository (path of the folder is the `<path-to-find-orphan>` used below).
+
+In your `~/.emacs`, add the following two lines:
+```Elisp
+(add-to-list 'load-path "<path-to-find-orphan>") ; add find-orphan to your load-path
+(require 'find-orphan)
+```
+
+## Usage
+* find-orphan-function-in-buffer : find orphan function in current buffer
+* find-orphan-function-in-directory : find orphan function in current directory
---
+++ find-orphan.el
@@ -0,0 +1,130 @@
+;;; find-orphan.el --- Find orphan function
+
+;; Filename: find-orphan.el
+;; Description: Find orphan function
+;; Author: Andy Stewart <lazycat.manatee@gmail.com>
+;; Maintainer: Andy Stewart <lazycat.manatee@gmail.com>
+;; Copyright (C) 2021, Andy Stewart, all rights reserved.
+;; Created: 2021-11-27 10:50:57
+;; Version: 0.1
+;; Last-Updated: 2021-11-27 10:50:57
+;;           By: Andy Stewart
+;; URL: https://www.github.org/manateelazycat/find-orphan
+;; Keywords:
+;; Compatibility: GNU Emacs 29.0.50
+;;
+;; Features that might be required by this library:
+;;
+;; `tree-sitter'
+;;
+
+;;; This file is NOT part of GNU Emacs
+
+;;; License
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+;;
+;; Find orphan function
+;;
+
+;;; Installation:
+;;
+;; Put find-orphan.el to your load-path.
+;; The load-path is usually ~/elisp/.
+;; It's set in your ~/.emacs like this:
+;; (add-to-list 'load-path (expand-file-name "~/elisp"))
+;;
+;; And the following to your ~/.emacs startup file.
+;;
+;; (require 'find-orphan)
+;;
+;; No need more.
+
+;;; Customize:
+;;
+;;
+;;
+;; All of the above can customize by:
+;;      M-x customize-group RET find-orphan RET
+;;
+
+;;; Change log:
+;;
+;; 2021/11/27
+;;      * First released.
+;;
+
+;;; Acknowledgements:
+;;
+;;
+;;
+
+;;; TODO
+;;
+;;
+;;
+
+;;; Require
+(require 'tree-sitter)
+
+;;; Code:
+
+(defun find-orphan-get-match-nodes (match-rule)
+  (ignore-errors
+    (let* ((query (tsc-make-query tree-sitter-language match-rule))
+           (root-node (tsc-root-node tree-sitter-tree))
+           (captures (mapcar #'cdr (tsc-query-captures query root-node #'tsc--buffer-substring-no-properties))))
+      captures)))
+
+(defun find-orphan-match-times-in-buffer (search-string)
+  (let ((match-count 0))
+    (save-excursion
+      (goto-char (point-min))
+      (while (search-forward search-string nil t)
+        (setq match-count (1+ match-count))))
+    match-count))
+
+(defun find-orphan-match-times-in-directory (search-string)
+  (let ((search-command (format "rg --no-ignore -g '!node_modules' -g '!dist' %s --stats -q" search-string)))
+    (string-to-number (nth 0 (split-string (nth 1 (split-string (shell-command-to-string search-command) "\n")))))))
+
+(defun find-orphan-function (match-times-func location)
+  (interactive)
+  (let* ((function-nodes (append (find-orphan-get-match-nodes "(function_definition name: (symbol) @x)")
+                                 (find-orphan-get-match-nodes "(function_definition name: (identifier) @x)")))
+         (function-names (mapcar #'tsc-node-text function-nodes))
+         (noreference-functions (remove-if-not #'(lambda (f) (<= (funcall match-times-func f) 1)) function-names)))
+    (if (> (length noreference-functions) 0)
+        (progn
+          (message "Found below orphan functions in current %s." location)
+          (message "--------")
+          (mapcar #'(lambda (f) (message "%s" f)) noreference-functions)
+          (message "--------"))
+      (message "Yay, no orphan function found in current %s." location))))
+
+(defun find-orphan-function-in-buffer ()
+  (interactive)
+  (find-orphan-function 'find-orphan-match-times-in-buffer "buffer"))
+
+(defun find-orphan-function-in-directory ()
+  (interactive)
+  (find-orphan-function 'find-orphan-match-times-in-directory "directory"))
+
+(provide 'find-orphan)
+
+;;; find-orphan.el ends here
